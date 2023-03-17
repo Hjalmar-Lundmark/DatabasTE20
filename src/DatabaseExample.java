2dcc81393090127fd215bf1dd01b47441b26cece
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2023-03-17 13:38:48 +0100

    It works, added more Queries

--- src/DatabaseExample.java
+++ src/DatabaseExample.java
@@ -4,22 +4,17 @@ import java.sql.*;
 public class DatabaseExample {
     public static void main(String[] args) {
         Connection conn = null;
-        String user = "magnus";
-        //String user = "te20";
+        //String user = "magnus";
         /*JPasswordField pf = new JPasswordField();
         JOptionPane.showConfirmDialog(null, pf, "password?", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
         String password = new String(pf.getPassword());*/
-        //String password = "6Xcwh5DQYLAe";
-        String password = "test";
-
+        //String password = "test";
 
         try {
-            //conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/bookshop? "+
+            conn = DriverManager.getConnection("jdbc:mysql://" + dbData.URL + ":3306/" + dbData.host + "? "+
+                    "allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",dbData.user,dbData.pwd);
+            //conn = DriverManager.getConnection("jdbc:mysql://10.70.45.159:3306/bookshop? "+
             //        "allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",user,password);
-            //conn = DriverManager.getConnection("jdbc:mysql://db.umea-ntig.se:3306/te20? "+
-            //        "allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",user,password);
-            conn = DriverManager.getConnection("jdbc:mysql://10.70.45.159:3306/bookshop? "+
-                    "allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",user,password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -41,11 +36,26 @@ public class DatabaseExample {
                 String output = "";
                 output += result.getInt("id") + ", " +
                         result.getString("title") + ", " +
-                        result.getString("author") + ", " +
-                        result.getDouble("price") + ", " +
-                        result.getInt("quantity");
+                        result.getString("content") + ", " +
+                        result.getTimestamp("createdAt") + ", " +
+                        result.getInt("authorId");
                 System.out.println(output);
             }
+
+            /*//INSERT command
+            SQLQuery = "INSERT INTO hl21forum(title,content) VALUES ('"+"HEJ"+"', '"+"brödtext"+"')";
+            stmt.executeUpdate(SQLQuery);
+            */
+
+            /*//UPDATE
+            SQLQuery = "UPDATE hl21forum SET title='"+"Tjena"+"' WHERE id="+2+"";
+            stmt.executeUpdate(SQLQuery);
+            */
+
+            /*//Full create post in my forum
+            SQLQuery = "INSERT INTO hl21forum(title,content,authorId) VALUES ('"+"Java post 2"+"', '"+"Mer innehåll från java"+"', "+2+")";
+            stmt.executeUpdate(SQLQuery);
+            */
 
             stmt.close();
             conn.close();
